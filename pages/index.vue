de415ef60963b41e1a33b7c797bea49beef506c1
Josh Hausotter <jhausotter@gmail.com>
2022-04-19 09:29:07 -0700

    add seo metadata for homepage

--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,26 @@ export default {
   name: "Homepage",
   components: {
     SliceZone
+  },
+  async asyncData({ $prismic, params, error }) {
+    const document = await $prismic.api.getSingle("home-page", params)
+    if (document) {
+      return { document }
+    } else {
+      error({ statusCode: 404, message: "Page not found" })
+    }
+  },
+  head() {
+    return {
+      title: this.document.data.meta_title,
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: this.document.data.meta_description,
+        }
+      ]
+    }
   }
 };
 </script>
